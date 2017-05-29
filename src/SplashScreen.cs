f3f3611e87b035e6f7f31a6229f4be787da3a6b9
Snark <ksp_snark@outlook.com>
2017-05-29 12:10:22 -0700

    Release v1.2.3. Update optional solar panels config to handle Kopernicus.

--- src/SplashScreen.cs
+++ src/SplashScreen.cs
@@ -13,6 +13,14 @@ namespace DefaultActionGroups
             "Configuring Default Action Groups..."
         };
 
+        /// <summary>
+        /// Snark's sneaky little way of thanking various users of this mod for helpful contributions.
+        /// </summary>
+        private static readonly string[] THANK_USERS =
+        {
+            "Errol"       // for spotting issue with solar panels & Kopernicus for KSP 1.3
+        };
+
         internal void Awake()
         {
             LoadingScreen.LoadingScreenState state = FindLoadingScreenState();
@@ -47,16 +55,16 @@ namespace DefaultActionGroups
         /// <param name="state"></param>
         private static void InsertTips(LoadingScreen.LoadingScreenState state)
         {
-            string[] newTips = new string[state.tips.Length + NEW_TIPS.Length];
-            for (int i = 0; i < state.tips.Length; ++i)
+            List<string> tipsList = new List<string>();
+            tipsList.AddRange(state.tips);
+            tipsList.AddRange(NEW_TIPS);
+            int numThanks = 1 + (int)Mathf.Sqrt(THANK_USERS.Length);
+            System.Random random = new System.Random(System.DateTime.UtcNow.Second);
+            for (int i = 0; i < numThanks; ++i)
             {
-                newTips[i] = state.tips[i];
+                tipsList.Add(string.Format("Thanking {0}...", THANK_USERS[random.Next(THANK_USERS.Length)]));
             }
-            for (int i = 0; i < NEW_TIPS.Length; ++i)
-            {
-                newTips[state.tips.Length + i] = NEW_TIPS[i];
-            }
-            state.tips = newTips;
+            state.tips = tipsList.ToArray();
         }
     }
 }
